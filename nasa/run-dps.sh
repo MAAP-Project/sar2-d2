0dcb89bc29506bfc4f95c1edfb65808f1af02897
Chuck Daniels <chuck@developmentseed.org>
2024-11-20 18:14:27 -0500

    Update nasa/run-dps.sh
    Co-authored-by: SamNemo <11642807+nemo794@users.noreply.github.com>

--- nasa/run-dps.sh
+++ nasa/run-dps.sh
@@ -4,4 +4,9 @@ set -Eeuo pipefail
 
 thisdir=$(dirname "$(readlink -f "$0")")
 
+# For DPS, hardcode the `SAR2D2_ENV` variable to match the
+# name of the custom environment in the environment.yml file
+# and in the build.sh script. This ensures we're using the correct
+# environment setup and dependencies during processing.
+# After, call the standard run.sh script.
 SAR2D2_ENV=sar2-d2 "${thisdir}"/run.sh "$@"
