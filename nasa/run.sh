23d15b31dd99a9b715baaa0ff0146111603120e6
niemoell <samantha.c.niemoeller@jpl.nasa.gov>
2024-11-21 13:20:56 -0800

    bugfix for dps

--- nasa/run.sh
+++ nasa/run.sh
@@ -29,51 +29,79 @@ positional arguments:
     exit 1
 fi
 
-if test -d "${input_dir}"; then
-    # RUNNING IN DPS
-    #
-    # There is an `input` sub-directory of the current working directory, so assume the
-    # input file is the sole file within the `input` sub-directory that the DPS
-    # automatically downloaded for us, based upon the URL given as the value of the
-    # in_file job input.
-    ls -l "${input_dir}" >&2
+# if test -d "${input_dir}"; then
+#     # RUNNING IN DPS
+#     #
+#     # There is an `input` sub-directory of the current working directory, so assume the
+#     # input file is the sole file within the `input` sub-directory that the DPS
+#     # automatically downloaded for us, based upon the URL given as the value of the
+#     # in_file job input.
+#     ls -l "${input_dir}" >&2
 
-    in_file="$(ls "${input_dir}"/*)"
-    bbox="$1"
-    in_type="$2"
-    out_type="$3"
-    posting="$4"
+#     in_file="$(ls "${input_dir}"/*)"
+#     bbox="$1"
+#     in_type="$2"
+#     out_type="$3"
+#     posting="$4"
+# else
+#     # RUNNING IN DEVELOPMENT ENVIRONMENT
+#     #
+#     # There is no `input` sub-directory of the current working directory, so simply
+#     # pass all arguments through to the Python script.  This is useful for testing in a
+#     # non-DPS environment, where there is no `input` directory since the DPS creates
+#     # the `input` directory.  In this case, we must pass a path to a "local" file,
+#     # which can be a path to a file in a shared bucket in the ADE.
+#     in_file="$1"
+
+#     if [[ $# -eq 8 ]]; then
+#         # Allow us to pass the coordinates as individual arguments, so we don't need to
+#         # remember to put quotes around them:
+#         #
+#         #   nasa/run.sh path/to/in-file LEFT BOTTOM RIGHT TOP  [...]
+#         #
+#         bbox="$2 $3 $4 $5"
+#         in_type="$6"
+#         out_type="$7"
+#         posting="$8"        
+#     else
+#         # Also allow us to put quotes around the 4 coordinates:
+#         #
+#         #   nasa/run.sh path/to/in-file "LEFT BOTTOM RIGHT TOP" [...]
+#         #
+#         bbox="$2"
+#         in_type="$3"
+#         out_type="$4"
+#         posting="$5"
+#     fi
+# fi
+
+## BEGIN HACK ##
+# HACK: DPS cannot download ALOS-1 data from ASF into input directory;
+# this is being handled internal to the algorithm for now.
+# We'll simply always pass in a http URL for the granule
+in_file="$1"
+
+if [[ $# -eq 8 ]]; then
+    # Allow us to pass the coordinates as individual arguments, so we don't need to
+    # remember to put quotes around them:
+    #
+    #   nasa/run.sh path/to/calibration-file LEFT BOTTOM RIGHT TOP
+    #
+    bbox="$2 $3 $4 $5"
+    in_type="$6"
+    out_type="$7"
+    posting="$8"        
 else
-    # RUNNING IN DEVELOPMENT ENVIRONMENT
+    # Also allow us to put quotes around the 4 coordinates:
     #
-    # There is no `input` sub-directory of the current working directory, so simply
-    # pass all arguments through to the Python script.  This is useful for testing in a
-    # non-DPS environment, where there is no `input` directory since the DPS creates
-    # the `input` directory.  In this case, we must pass a path to a "local" file,
-    # which can be a path to a file in a shared bucket in the ADE.
-    in_file="$1"
-
-    if [[ $# -eq 8 ]]; then
-        # Allow us to pass the coordinates as individual arguments, so we don't need to
-        # remember to put quotes around them:
-        #
-        #   nasa/run.sh path/to/calibration-file LEFT BOTTOM RIGHT TOP
-        #
-        bbox="$2 $3 $4 $5"
-        in_type="$6"
-        out_type="$7"
-        posting="$8"        
-    else
-        # Also allow us to put quotes around the 4 coordinates:
-        #
-        #   nasa/run.sh path/to/calibration-file "LEFT BOTTOM RIGHT TOP"
-        #
-        bbox="$2"
-        in_type="$3"
-        out_type="$4"
-        posting="$5"
-    fi
+    #   nasa/run.sh path/to/calibration-file "LEFT BOTTOM RIGHT TOP"
+    #
+    bbox="$2"
+    in_type="$3"
+    out_type="$4"
+    posting="$5"
 fi
+## END HACK ##
 
 # WARNING: DO NOT place quotes around ${bbox} in the following command.  The
 # bbox should be 4 coordinates, each as individual (repeated) bbox arguments,
