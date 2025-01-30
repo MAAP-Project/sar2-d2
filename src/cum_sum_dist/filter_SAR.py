2d869644552ec31c289d8693687436b4a6417f83
Samantha C. Niemoeller <samantha.c.niemoeller@jpl.nasa.gov>
2025-01-30 13:21:39 -0800

    Remove dependency on Constants

--- src/cum_sum_dist/filter_SAR.py
+++ src/cum_sum_dist/filter_SAR.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 from scipy import ndimage, signal
 from scipy.interpolate import griddata
-from skimage.restoration import denoise_tv_chambolle, denoise_tv_bregman
-
-from dswx_sar.dswx_sar_util import Constants
+from skimage.restoration import denoise_tv_bregman, denoise_tv_chambolle
 
 K_DEFAULT = 1.0
 CU_DEFAULT = 0.523
@@ -62,7 +60,7 @@ def compute_window_mean_std(arr, winsize):
 
     # The negative number in sqrt is replaced
     # with the negligibly small number to avoid numpy warning message.
-    var = np.where(var < 0, Constants.negligible_value, var)
+    var = np.where(var < 0, 1e-7, var)
     std = var**0.5
 
     return mean, std
